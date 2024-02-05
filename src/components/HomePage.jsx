6e138004b57bfedd8498360ee3465697d7bb07f7
ranveer <ranveersinghsingh2644@gmail.com>
2024-02-05 14:52:34 +0530

    Updated files

---
+++ src/components/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import LeftHome from "./LeftHome";
+import './HomeStyles.css';
+
+const HomePage = () => {
+  return (
+    <div>
+      <div className='parent'>
+       <LeftHome />
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
